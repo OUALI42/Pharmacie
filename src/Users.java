7cfef7b3cdeca286ceec90f4579849a2a71feed1
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-01-30 16:43:57 +0100

    Added AddUser function

--- src/Users.java
+++ src/Users.java
@@ -42,4 +42,9 @@ public abstract class Users {
             }
         }
     }
+
+    public void AddUser(String name, String password) {
+        names.add(name);
+        passwords.add(password);
+    }
 }
