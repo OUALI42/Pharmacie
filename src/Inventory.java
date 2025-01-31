25b3c612e4cadf7aeb4c00cb91193fe6dce0ad3a
Shado <anais.q33@gmail.com>
2025-01-31 10:55:55 +0100

    Add java doc and comments to addProduct, removeProduct and class Inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,6 +1,9 @@
 import java.util.List;
-import java.util.Scanner;
 
+/**
+ * Class Inventory containing a list of products.
+ * Inventory is associated with a category and a sub-category.
+ */
 public class Inventory {
     public String categorie;
     public String sousCategorie;
@@ -8,22 +11,33 @@ public class Inventory {
 
     Inventory(){
     }
+
+    /**
+     * Constructor with parameters.
+     * @param categorie The product category.
+     * @param sousCategorie The product's sub-category.
+     * @param produits The list of products associated with this category.
+     */
     public Inventory(String categorie, String sousCategorie, List<Product> produits) {
         this.categorie = categorie;
         this.sousCategorie = sousCategorie;
         this.produits = produits;
     }
 
+    /**
+     * Defines product list.
+     * @param produits New product list.
+     */
     public void setProduits(List<Product> produits) {
         this.produits = produits;
     }
 
+    /**
+     * Returns the list of products contained in the inventory.
+     * @return produits list.
+     */
     public List<Product> getProduits() {
         return produits;
-    }
-
-    public void removeObject(Product productRm) {
-        produits.remove(productRm);
     }
 
 }
